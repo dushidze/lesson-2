54b313e4997cb093550161b949dac0a2296ea062
Casey Jones <dushidze.crpty@gmail.com>
2026-03-17 13:59:03 +0200

    added conditions theory

--- lesson2.py
+++ lesson2.py
@@ -57,3 +57,49 @@
 #      print("qwerty")
 # else: #else - крайний случай и никогда не пишем условий
 #      print("AM")
+
+# Пользователь вводит три цифры с клавиатуры. Зависимо от выбора пользователя программа выводит на экран максимум
+# из трех, минимум из трех или среднее арифметическое трех чисел.
+
+# n1 = int(input("Enter first number: "))
+# n2 = int(input("Enter second number: "))
+# n3 = int(input("Enter third number: "))
+#
+# print(f"Your numbers is: Number1 = {n1} Number2 = {n2} Number3 = {n3}")
+#
+# print("Choose operation")
+# print("1. Max number")
+# print("2. Min number")
+# print("3. Avg number")
+#
+# choice = int(input("Enter your choice: "))
+#
+# if choice == 1:
+#         print("The maximum number is ", max(n1, n2, n3))
+# elif choice == 2:
+#         print("The minimum number is ", min(n1, n2, n3))
+# elif choice == 3:
+#         print("The average number is ", (n1 + n2 + n3) / 3)
+# else:
+#         print("Invalid choice")
+
+# Пользователь вводит клавиатурой количество метров. В зависимости от выбора пользователя программа переводит
+# в метры мили, дюймы или ярды.
+
+# metres = int(input("Enter the number of metres: "))
+#
+# print("Choose action")
+# print("1. Miles")
+# print("2. Inch")
+# print("3. Yards")
+#
+# choice = int(input("Enter your choice: "))
+#
+# if choice == 1:
+#     print("Miles = ", metres / 1609.344)
+# elif choice == 2:
+#     print("Inch = ", metres * 39.37)
+# elif choice == 3:
+#     print("Yards = ", metres / 0.9144)
+# else:
+#     print("Invalid choice")
